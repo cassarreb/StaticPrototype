37ac4307f41477f7978c22872631f0b08e4060e5
rebecca-kai.cassar.13@um.edu.mt <rebecca-kai.cassar.13@um.edu.mt>
2017-09-11 21:54:53 +0200

    tracker

--- public/torr-per-app-browserified.js
+++ public/torr-per-app-browserified.js
@@ -28425,7 +28425,7 @@ announceList = [
  ['udp://tracker.coppersurfer.tk:6969'],
  ['udp://exodus.desync.com:6969'],
  ['wss://tracker.btorrent.xyz'],
-// ['wss://tracker.openwebtorrent.com']
+ ['wss://tracker.openwebtorrent.com']
  ]
 
 var opts = {
@@ -28447,8 +28447,7 @@ function logLocalStorage(message, req_no, status, timestamp) {
 }
     //server aws: 5b9cbb7e198d501278906b977164ea57683e2266
 var path = "79d2a7a81026164d65f7d8e7958375f5b972faa7";
-var magnet = 'magnet:?xt=urn:btih:' + path + '&dn=Unnamed+Torrent+1476541118022&tr=udp%3A%2F%2Fexodus.desync.com%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80';
-    //&tr=wss%3A%2F%2Ftracker.openwebtorrent.com'
+var magnet = 'magnet:?xt=urn:btih:' + path + '&dn=Unnamed+Torrent+1476541118022&tr=udp%3A%2F%2Fexodus.desync.com%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=wss%3A%2F%2Ftracker.openwebtorrent.com';
 
 torrent = client.add(magnet, onTorrent);
 var start = Date.now();
