82143218df7ac970c4e9873953d277b72b823657
rebecca-kai.cassar.13@um.edu.mt <rebecca-kai.cassar.13@um.edu.mt>
2017-09-14 18:49:01 +0200

    m

--- public/torr-per-app-browserified.js
+++ public/torr-per-app-browserified.js
@@ -28635,39 +28635,39 @@ function onTorrent(torrent) {
     })
 }
 
-setInterval(function () {
-    //Ideally, here we stop the torrent from downloading
-    var end = Date.now();
-    var scriptTags = Array.from(document.getElementsByTagName("script"));
-    var imageTags = Array.from(document.getElementsByTagName("img"));
-    var styleTags = Array.from(document.getElementsByTagName("link"));
+//setInterval(function () {
+//    //Ideally, here we stop the torrent from downloading
+//    var end = Date.now();
+//    var scriptTags = Array.from(document.getElementsByTagName("script"));
+//    var imageTags = Array.from(document.getElementsByTagName("img"));
+//    var styleTags = Array.from(document.getElementsByTagName("link"));
 
-    if (end - start > 10000) {
-        //for scripts, check each one and add them after each other
-        scriptTags.forEach(function (listItem, index) {
-            if (listItem.getAttribute("src") == null || listItem.getAttribute("src") == "#") {
-                listItem.setAttribute("src", listItem.getAttribute("altern"));
-                console.log("set");
-            }
-        })
+//    if (end - start > 10000) {
+//        //for scripts, check each one and add them after each other
+//        scriptTags.forEach(function (listItem, index) {
+//            if (listItem.getAttribute("src") == null || listItem.getAttribute("src") == "#") {
+//                listItem.setAttribute("src", listItem.getAttribute("altern"));
+//                console.log("set");
+//            }
+//        })
 
-        styleTags.forEach(function (listItem, index) {
-            if (listItem.getAttribute("href") == null || listItem.getAttribute("href") == "#") {
-                listItem.setAttribute("href", listItem.getAttribute("altern"));
+//        styleTags.forEach(function (listItem, index) {
+//            if (listItem.getAttribute("href") == null || listItem.getAttribute("href") == "#") {
+//                listItem.setAttribute("href", listItem.getAttribute("altern"));
                 
-                console.log("set");
-            }
-        })
+//                console.log("set");
+//            }
+//        })
 
-        imageTags.forEach(function (listItem, index) {
-            if (listItem.getAttribute("src") == null || listItem.getAttribute("src") == "#") {
-                listItem.setAttribute("src", listItem.getAttribute("altern"));
+//        imageTags.forEach(function (listItem, index) {
+//            if (listItem.getAttribute("src") == null || listItem.getAttribute("src") == "#") {
+//                listItem.setAttribute("src", listItem.getAttribute("altern"));
                
-                console.log("set");
-            }
-        })
-    }
-}, 3000);
+//                console.log("set");
+//            }
+//        })
+//    }
+//}, 3000);
 
 
 (function (console) {
